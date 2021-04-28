ffe5c12996d51b76719d26fa1f5ec89f874cbfc8
Gabriel Batista <46146420+lksaber@users.noreply.github.com>
2021-04-27 21:16:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,36 @@
 <?php
-    include("conn.php");
-
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // collect value of input field
         $path = $_POST['path'];
-        var_dump($path);
         if($path=="login"){
-
             echo login();
+        }else{
+            echo 'Error ao chamar Login';
         }
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        // collect value of input field
         $path = $_GET['path'];
-        if (empty($path)) {
-        echo "path is empty";
-        } else {
-        echo $path;
+        if($path=="login"){
+            echo login();
         }
     }
 
+// Função de login para o usuario 
+function login(){
+    $conn =  mysqli_connect("aurorahorizon.ddns.net:3306", "gabriel", "teste12345678", "aps4sem");
+        if (!$conn) {
+            die('Não foi possível conectar: ' );            
+        }      
 
+    $sql = "SELECT ID_USUARIO, NOME, APELIDO, DATA_NASC, SENHA, LOGIN, EMAIL FROM usuario";
+    $result = $conn->query($sql);
 
-        function login(){
-            $conn =  mysqli_connect("aurorahorizon.ddns.net:3306", "gabriel", "teste12345678", "aps4sem");
-            $sql = "SELECT ID_USUARIO, NOME, APELIDO, DATA_NASC, SENHA, LOGIN, EMAIL FROM usuario";
-            $result = $conn->query($sql);
-            while($row = $result->fetch_assoc()) {
+    while($row = $result->fetch_assoc()) {
                 echo $row["NOME"]. $row["APELIDO"]. " " . $row["DATA_NASC"]."<br>";
                 $arr = array($row["NOME"] => 1, $row["APELIDO"] => 2, $row["DATA_NASC"] => 3);
                 echo json_encode($arr, JSON_UNESCAPED_UNICODE);
-            }
-         }
+    }
+}
 
 
 ?>
